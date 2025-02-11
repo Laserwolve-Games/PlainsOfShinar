ac4ce571417141f1adbdf6e3934c5662a3abdb8d
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-02-11 11:46:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const app = new PIXI.Application();
-await app.init({width: 960, height: 540});
+(async () => {
+    const app = new PIXI.Application();
+    await app.init({width: 960, height: 540});
 
-document.body.appendChild(app.canvas);
+    document.body.appendChild(app.canvas);
 
-await PIXI.Assets.load([ "spritesheets/man.json", "background.webp" ]);
+    await PIXI.Assets.load([ "spritesheets/man.json", "background.webp" ]);
 
-const animations = PIXI.Assets.cache.get('spritesheets/man.json').data.animations;
-console.log(animations);
+    const animations = PIXI.Assets.cache.get('spritesheets/man.json').data.animations;
+    console.log(animations);
 
-const background = PIXI.Sprite.from("background.webp");
-app.stage.addChild(background);
+    const background = PIXI.Sprite.from("background.webp");
+    app.stage.addChild(background);
 
-app.stage.scale.x = app.canvas.width / background.width;
-app.stage.scale.y = app.canvas.height / background.height;
+    app.stage.scale.x = app.canvas.width / background.width;
+    app.stage.scale.y = app.canvas.height / background.height;
 
-const character = PIXI.AnimatedSprite.fromFrames(animations["die/die_0"]);
+    const character = PIXI.AnimatedSprite.fromFrames(animations["die_0"]);
 
-character.animationSpeed = .5;
-character.position.set(150, background.height - 780);
-character.play();
+    character.animationSpeed = .5;
+    character.position.set(150, background.height - 780);
+    character.play();
 
-character.updateAnchor =true;
+    character.updateAnchor =true;
 
-app.stage.addChild(character);
+    app.stage.addChild(character);
+})();

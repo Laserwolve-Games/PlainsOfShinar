07d74cbf296bb43293007930b22bb1d78d737f28
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-02-14 13:05:46 -0400

    hold mouse to move and isometric speed

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ let entities = [];
 
     document.body.appendChild(app.canvas);
 
-    await loadAsset("background.webp");
+    await loadAsset('background.webp');
 
     // const animations = PIXI.Assets.cache.get('spritesheets/man.json').data.animations;
 
-    const background = PIXI.Sprite.from("background.webp");
+    const background = PIXI.Sprite.from('background.webp');
     app.stage.addChild(background);
 
     app.stage.scale.x = app.canvas.width / background.width;
@@ -26,12 +26,18 @@ let entities = [];
 
     const player = new Player();
 
-    app.canvas.addEventListener('pointerdown', (event) => {
+    let PointerIsDown = false;
+    let mouseX = 0;
+    let mouseY = 0;
+
+    app.canvas.addEventListener('pointermove', (event) => {
         const rect = app.canvas.getBoundingClientRect();
-        const targetX = (event.clientX - rect.left) / app.stage.scale.x;
-        const targetY = (event.clientY - rect.top) / app.stage.scale.y;
-        player.moveTo(targetX, targetY);
+        mouseX = (event.clientX - rect.left) / app.stage.scale.x;
+        mouseY = (event.clientY - rect.top) / app.stage.scale.y;
     });
+
+    app.canvas.addEventListener('pointerdown', () => PointerIsDown = true);
+    app.canvas.addEventListener('pointerup', () => PointerIsDown = false);
 
     // stuff that runs every tick
     app.ticker.add(() => {
@@ -40,10 +46,10 @@ let entities = [];
         // and maybe making it an oval and adjusting its angle.
         // player.body.position.set(player.position.x, player.position.y);
 
+        if (PointerIsDown) player.moveTo(mouseX, mouseY);
+
         // every tick, for every entity
         entities.forEach(entity => {
-
-
 
             // entity movement
             if (entity.targetPosition !== entity.position) {
@@ -52,12 +58,15 @@ let entities = [];
                 const dy = entity.targetPosition.y - entity.position.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
 
-                if (distance > entity.speed) {
+                entity.actualSpeed = isometrify(entity.speed, entity.actualFacing);
+                console.log(entity.actualSpeed);
+
+                if (distance > entity.actualSpeed) {
 
                     entity.isMoving = true;
 
-                    entity.position.x += (dx / distance) * entity.speed;
-                    entity.position.y += (dy / distance) * entity.speed;
+                    entity.position.x += (dx / distance) * entity.actualSpeed;
+                    entity.position.y += (dy / distance) * entity.actualSpeed;
 
                 } else {
 
@@ -66,47 +75,36 @@ let entities = [];
                     entity.targetPosition = entity.position;
                 }
             }
-
-            // TODO: I don't think we need to do this every tick, but let's figure that out when we deal with holding click to move
             if (entity.isMoving) {
 
-                entity.facing = calculateFacing(entity.position.x, entity.position.y, entity.targetPosition.x, entity.targetPosition.y);
+                entity.calculateFacing(entity.position.x, entity.position.y, entity.targetPosition.x, entity.targetPosition.y);
 
-                const animation = "walk_" + entity.facing;
+                // If the entity already was walking, play from the current frame, otherwise start from the beginning
+                if (entity.body.animation.includes('walk')) entity.setAnimation('walk_' + entity.facing, false);
+                else entity.setAnimation('walk_' + entity.facing, true);
 
-                if (entity.body.animation != animation) {
-
-                    entity.setAnimation("walk_" + entity.facing);
-                }
-            } else {
-
-                const animation = "idle_" + entity.facing;
-
-                if (entity.body.animation != animation) {
-
-                    entity.setAnimation("idle_" + entity.facing);
-                }
-            }
+            } else entity.setAnimation('idle_' + entity.facing, true);
 
             // keep bodies on top of their entities
             entity.body.position.set(entity.position.x, entity.position.y);
         });
     });
 })();
-
 class Entity extends PIXI.Sprite {
 
-    constructor(spritesheet, animation, width, height, x, y, facing) {
+    constructor(spritesheet, animation, size, x, y, facing) {
 
         spritesheet = 'spritesheets/' + spritesheet + '.json';
 
         super(PIXI.Texture.WHITE);
-        this.width = width;
-        this.height = height;
+
+        // this.visible = false;
+        this.width = size;
+        this.height = this.width / 2;
         this.position.set(x, y);
         this.facing = facing;
+        this.actualFacing = facing;
         this.targetPosition = this.position;
-
         this.speed = 0;
         this.anchor.set(.5);
 
@@ -118,61 +116,78 @@ class Entity extends PIXI.Sprite {
 
         this.spritesheet = PIXI.Assets.cache.get(spritesheet).data.animations;
 
-        this.setAnimation(animation);
+        this.setAnimation(animation, true);
 
         app.stage.addChild(this);
 
         entities.push(this);
     }
-    moveTo(x, y) {
+    moveTo = (x, y) => {
 
         this.targetPosition = { x, y };
     }
-    setAnimation(animation) {
+    setAnimation = (animation, playFromBeginning) => {
 
-        if (this.body) {
-            app.stage.removeChild(this.body);
-            this.body.destroy();
+        // Don't set the animation if the new one is the same as the current one
+        if (this.body?.animation != animation) {
+
+            // Save the current frame of the current animation before destroying it,
+            // to use if playFromBeginning is false
+            let savedFrame;
+
+            // Get rid of the current wrong animation, if there is one
+            if (this.body) {
+
+                if (!playFromBeginning) savedFrame = this.body.currentFrame;
+
+                app.stage.removeChild(this.body);
+
+                this.body.destroy();
+            }
+            this.body = PIXI.AnimatedSprite.fromFrames(this.spritesheet[animation]);
+            this.body.animation = animation;
+            this.body.animationSpeed = .5;
+            this.body.updateAnchor = true;
+
+            if (playFromBeginning) this.body.currentFrame = 0;
+            else this.body.currentFrame = savedFrame;
+
+            this.body.play();
+
+            // Add the new animated sprite to the stage
+            app.stage.addChild(this.body);
         }
-        this.body = PIXI.AnimatedSprite.fromFrames(this.spritesheet[animation]);
-        this.body.animation = animation;
-        this.body.animationSpeed = .5;
-        this.body.updateAnchor = true;
-        if (!this.body.playing) this.body.play();
+    }
+    calculateFacing = (x1, y1, x2, y2) => {
 
-        // Add the new animated sprite to the stage
-        app.stage.addChild(this.body);
+        const deltaX = x2 - x1;
+        const deltaY = y2 - y1;
+        const angleInRadians = Math.atan2(deltaY, deltaX);
+        const angleInDegrees = angleInRadians * (180 / Math.PI);
+
+        this.actualFacing = angleInDegrees;
+
+        let facing = Math.round(angleInDegrees / 22.5) * 22.5;
+    
+        facing = Math.abs((facing + 270) % 360 - 360);
+        if (facing == 360) facing = 0;
+
+        this.facing = facing;
     }
 }
-
 class Player extends Entity {
 
     constructor() {
 
-        super('man', 'walk', 50, 25, 150, 300, 90);
+        super('man', 'idle', 50, 150, 300, 90);
 
         this.speed = 5;
     }
 }
-async function loadAsset(asset) {
-
-    if (!PIXI.Assets.cache.has(asset)) {
-
-        await PIXI.Assets.load(asset);
-    }
+const loadAsset = async (asset) => {
+    
+    if (!PIXI.Assets.cache.has(asset)) await PIXI.Assets.load(asset);
 }
-function calculateFacing(x1, y1, x2, y2) {
+const isometrify = (value, angle) => value * lerp(.5, 1, Math.abs(Math.abs(angle) - 90) / 90);
 
-    const deltaX = x2 - x1;
-    const deltaY = y2 - y1;
-    const angleInRadians = Math.atan2(deltaY, deltaX);
-    const angleInDegrees = angleInRadians * (180 / Math.PI);
-    // TODO: Correct facings in the DazScript
-    let facing = Math.round(angleInDegrees / 22.5) * 22.5;
-
-    // Code to correct the facings if they weren't corrected in the DazScript
-    facing = Math.abs((facing + 270) % 360 - 360);
-    if (facing == 360) facing = 0;
-
-    return facing;
-}
+const lerp = (a, b, x) => a + x * (b - a);
